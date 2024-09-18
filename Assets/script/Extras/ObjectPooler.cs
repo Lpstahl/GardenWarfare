94d5c775328dd3652a2034b24e07a536ba29db60
Lpstahl <104699734+Lpstahl@users.noreply.github.com>
2024-09-18 14:18:18 -0300

    Spawner Pooler

---
+++ Assets/script/Extras/ObjectPooler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Pooler é como uma fábrica de objetos que mantém uma reserva (pool) de objetos prontos para uso, em vez de criar e descartar objetos o tempo todo.
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab; // Define o prefab a ser instanciado.
+    [SerializeField] private int poolSize = 10; // Define o tamanho do pool.
+
+    private List<GameObject> _pool; // Define a lista de GameObjects.
+    private GameObject _poolContainer; // Define o container do pool.
+
+    private void Awake()
+    {
+        _pool = new List<GameObject>(); // Inicializa a lista de GameObjects.
+        _poolContainer = new GameObject($"Pool - {prefab.name}"); // Cria o container do pool.
+        CreatePooler(); // Cria o pooler.
+    }
+
+    private void CreatePooler()
+    {
+       for (int i = 0; i < poolSize; i++) // Para cada GameObject no pool,
+        {
+            _pool.Add(CreateInstance()); // Adiciona um novo GameObject.
+        }
+    }
+
+    private GameObject CreateInstance()
+    {
+        GameObject newInstance = Instantiate(prefab); // Instancia um novo GameObject.
+        newInstance.transform.SetParent(_poolContainer.transform); // Define o container do pool como o pai do GameObject.
+        newInstance.SetActive(false); // Desativa o GameObject.
+        return newInstance; // Retorna o GameObject.
+    }
+
+    public GameObject GetInstanceFromPool()
+    {
+        for(int i = 0; i < _pool.Count; i++) // Para cada GameObject no pool,
+        {
+            if (!_pool[i].activeInHierarchy) // Se o GameObject não estiver ativo,
+            {
+                return _pool[i]; // Retorna o GameObject.
+            }
+        }
+                return CreateInstance(); // Retorna um novo GameObject.
+    }
+}
--- Assets/script/Spawner/Spawner.cs
+++ Assets/script/Spawner/Spawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SpawnType
+public enum SpawnType // Define o tipo de spawn.
 {
     Fixed,
     Random
@@ -11,57 +11,65 @@ public enum SpawnType
 public class Spawner : MonoBehaviour
 {
     [Header("Spawner Settings")]
-    [SerializeField] private SpawnType spawnType = SpawnType.Fixed;
-    [SerializeField] private int enemyCount = 10;
-    [SerializeField] private GameObject testGO;
+    [SerializeField] private SpawnType spawnType = SpawnType.Fixed; // Define o tipo de spawn.
+    [SerializeField] private int enemyCount = 10; // Define o número de inimigos.
+    [SerializeField] private GameObject testGO; //  Define o GameObject de teste.
 
     [Header("Fixed Spawn")]
-    [SerializeField] private float delayBtwSpawns;
+    [SerializeField] private float delayBtwSpawns; // Define o delay entre spawns.
 
     [Header("Random Spawn")]
-    [SerializeField] private float minRandomDelay;
-    [SerializeField] private float maxRandowDelay;
+    [SerializeField] private float minRandomDelay; // Define o mínimo de delay aleatório.
+    [SerializeField] private float maxRandowDelay; // Define o máximo de delay aleatório.
 
-    private float _spawnTimer;
-    private int _enemiesSpawned;
+    private float _spawnTimer; // Define o timer de spawn.
+    private int _enemiesSpawned; // Define o número de inimigos spawnados.
+
+    private ObjectPooler _pooler;// Define o pooler como o componente ObjectPooler.
+
+    private void Start()
+    {
+        _pooler = GetComponent<ObjectPooler>(); // Define o pooler como o componente ObjectPooler.  
+    }
 
     // Update is called once per frame
     void Update()
     {
-        _spawnTimer -= Time.deltaTime;
-        if (_spawnTimer < 0)
+        _spawnTimer -= Time.deltaTime; // Decrementa o timer de spawn.
+        if (_spawnTimer < 0) // Se o timer de spawn for menor que 0,
         {
-            _spawnTimer = GetSpawnDelay();
-            if (_enemiesSpawned < enemyCount)
+            _spawnTimer = GetSpawnDelay(); // Define o timer de spawn como o delay de spawn.
+            if (_enemiesSpawned < enemyCount) // Se o número de inimigos spawnados for menor que o número de inimigos,
             {
-                _enemiesSpawned++;
-                SpawnEnemy();
+                _enemiesSpawned++; // Incrementa o número de inimigos spawnados.
+                SpawnEnemy(); // Spawna um inimigo.
             }
         }
     }
 
     private void SpawnEnemy()
     {
-        Instantiate(testGO, transform.position, Quaternion.identity);
+        GameObject newInstance = _pooler.GetInstanceFromPool(); // Instancia um novo GameObject.
+        newInstance.SetActive(true); // Ativa o GameObject.
     }
 
     private float GetSpawnDelay()
     {
-        float delay = 0;
-        if (spawnType == SpawnType.Fixed)
+        float delay = 0; // Define o delay como 0.
+        if (spawnType == SpawnType.Fixed) // Se o tipo de spawn for fixo,
         {
-            delay = delayBtwSpawns;
+            delay = delayBtwSpawns; // Define o delay como o delay entre spawns.
         }
-        else if (spawnType == SpawnType.Random)
+        else // Se o tipo de spawn for aleatório,
         {
-            delay = GetRandomDelay();
+            delay = GetRandomDelay(); // Define o delay como um delay aleatório.
         }
-        return delay;
+        return delay; // Retorna o delay.
     }
 
     private float GetRandomDelay()
     {
-        float randomTimer = Random.Range(minRandomDelay, maxRandowDelay);
-        return randomTimer;
+        float randomTimer = Random.Range(minRandomDelay, maxRandowDelay); // Define o timer aleatório como um valor aleatório entre o mínimo e o máximo.
+        return randomTimer; // Retorna o timer aleatório.
     }
 }
