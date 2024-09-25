f72691e4beb77edb689398cd830b8034d43ee9d4
Lpstahl <104699734+Lpstahl@users.noreply.github.com>
2024-09-25 14:55:43 -0300

    EnemyHealth / healthBar

--- Assets/script/Enemy/enemy.cs
+++ Assets/script/Enemy/enemy.cs
@@ -1,12 +1,23 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public static Action OnEndReached; // Define a ação OnEndReached.
 
     [SerializeField] public float movespeed = 2f;
-    [SerializeField] public float health = 100f; // saúde do inimigo
+    
+    [Header("Health")]
+    [SerializeField] public float initialHealth = 100f; // saúde do inimigo
+    [SerializeField] public float maxHealth = 100f; // saúde máxima do inimigo
+    
+    [SerializeField] private GameObject healthBarPrefab; // Prefab da barra de vida.
+    [SerializeField] private Transform barPosition; // Posição da barra de vida.
+
+    public float CurrentHealth { get; set; }
+
+    private Image _helthBar; // Barra de vida do inimigo.
 
     public Waypoint Waypoint { get; set; } // Define o waypoint.
 
@@ -14,10 +25,13 @@ public class Enemy : MonoBehaviour
 
     private int _currentWaypointIndex; // Define o índice do waypoint atual.
 
+
     private void Start()
     {
         _currentWaypointIndex = 0; // Define o índice do waypoint atual como 0.
-        
+
+        CreateHealthBar(); // Cria a barra de vida.
+        CurrentHealth = initialHealth; // Define a saúde atual como a saúde inicial.
     }
 
     private void Update()
@@ -27,6 +41,7 @@ public class Enemy : MonoBehaviour
         {
             UpdateCurrentPointIndex();  // Atualiza o índice do ponto atual.
         }
+  
     }
 
     private void Move()
@@ -64,10 +79,20 @@ public class Enemy : MonoBehaviour
         _currentWaypointIndex = 0; // Define o índice do waypoint atual como 0.
     }
 
+    private void CreateHealthBar()
+    {
+        GameObject newBar = Instantiate(healthBarPrefab, barPosition.position, Quaternion.identity); // Instancia uma nova barra de vida.
+        newBar.transform.SetParent(transform); // Define a barra de vida como filha do inimigo.
+
+        EnemyHealthContainer healthContainer = newBar.GetComponent<EnemyHealthContainer>(); // Obtém o componente EnemyHealthContainer da barra de vida.
+        _helthBar = healthContainer.FillAmountImage; // Define a barra de vida.
+    }
+
     public void TakeDamage(float amount)
     {
-        health -= amount; // Decrementa a saúde do inimigo.
-        if (health <= 0) // Se a saúde do inimigo for menor ou igual a 0,
+        initialHealth -= amount; // Decrementa a saúde do inimigo.
+        _helthBar.fillAmount = initialHealth / maxHealth; // Atualiza a barra de vida.
+        if (initialHealth <= 0) // Se a saúde do inimigo for menor ou igual a 0,
         {
             Die(); // Morre.
         }
@@ -75,7 +100,8 @@ public class Enemy : MonoBehaviour
 
     private void Die()
     {
-        // Adicione efeitos de morte aqui
-      ObjectPooler.ReturnToPool(gameObject); // Retorna o inimigo para o pool.
+        CurrentHealth = maxHealth; // Define a saúde atual como a saúde máxima.
+        _helthBar.fillAmount = 1f; // Atualiza a barra de vida.
+        ObjectPooler.ReturnToPool(gameObject); // Retorna o inimigo para o pool.
     }
 }
